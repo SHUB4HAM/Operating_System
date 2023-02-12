86ed17cd62c4800bb190239bd5de9d2d23df70da
SHUB4HAM <shubhamdhoke23@gmail.com>
2023-02-12 23:10:34 +0530

    Run option was not showing in IDE. Issue was -> main(String args)    ... wrong main(String[] args)  ... correct one

--- src/OS5/Topic1/Main.java
+++ src/OS5/Topic1/Main.java
@@ -1,10 +1,10 @@
 package OS5.Topic1;
 
 public class Main {
-    public static void main(String args){
+    public static void main(String[] args){
         Count c = new Count();
         Thread t1 = new Thread(new Adder(c));
-        Thread t2 = new Thread(new Substractor(c));
+//        Thread t2 = new Thread(new Substractor(c));
 
         System.out.println(c.getValue());
     }
